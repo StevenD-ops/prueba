f50273bf1110ca96d4170d544a9ece93fb925460
StevenD-ops <81279493+StevenD-ops@users.noreply.github.com>
2021-05-13 02:18:26 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,27 @@
+# SteeveBot
+Pasos para instalar el bot via termux (para ubuntu utilizar npm install)
+
+pkg install git
+
+pkg install ffmpeg
+
+pkg install wget
+
+pkg install nodejs
+
+pkg install npm
+
+git clone https://github.com/StevenD-ops/WhatsappBot-v1.git
+
+cd WhatsappBot-v1
+
+bash install.sh
+
+npm start
+
+Escaneas el QR y listo comando para activarlo  *menu
+
+## whatsapp numero
+* [`WhatsApp`](wa.me/18299416234)
+### Quieres aportar a la causa
+*  18299416234
---
+++ src/help.js
@@ -0,0 +1,64 @@
+const help = (prefix) => {
+	return `
+「 *Bender Bot* 」
+
+◪ *Información*
+❏ Comando: 「  ${prefix}  」
+❏ Creador: StevenD
+❏ Como instalar el bot: Solo agregre a sus contacto a Botcito y registrese.
+❏ Bot aun en Desarrollo. tengan paciencia, pronto vendran mas actualizaciones
+	❏ Cualquier inconvendiente que presente favor de cominicar
+
+◪ *Sticker*
+  │
+  ├─ ❏ ${prefix}sticker
+  ├─ ❏ ${prefix}stickergif
+  ├─ ❏ ${prefix}toimg
+  └─ ❏ ${prefix}tomp3
+
+◪ *DESCARGAS DE YOUTUBE*
+  │
+  └─ ❏ ${prefix}ytmp4
+
+◪ *AUDIO *
+  │
+  └─ ❏ ${prefix}tts
+
+◪ *ADMIN*
+  │
+  ├─ ❏ ${prefix}menuadmin
+  ├─ ❏ ${prefix}wame
+  ├─ ❏ ${prefix}qrcode
+  ├─ ❏ ${prefix}toinmenu
+
+	hermano tengo hambre
+	hazme una donación
+	+1(829)941-6234
+
+`
+}
+/*
+◪ *SOBRE*
+├─ ❏ ${prefix}setprefix
+├─ ❏ ${prefix}block
+├─ ❏ ${prefix}bc
+├─ ❏ ${prefix}bcgc
+├─ ❏ ${prefix}clone
+└─ ❏ ${prefix}clearall
+  │  ├─ ❏ ${prefix}send
+	├─ ❏ ${prefix}virtex (Hernqn)
+	├─ ❏ ${prefix}exe
+	├─ ❏ ${prefix}afk
+	├─ ❏ ${prefix}timer
+	├─ ❏ ${prefix}fml
+	└─ ❏ ${prefix}fml2
+  ├─ ❏ ${prefix}blocklist
+  ├─ ❏ ${prefix}ping
+  └─ ❏ ${prefix}bugreport
+
+
+
+
+
+	*/
+exports.help = help
